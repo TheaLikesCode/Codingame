fbb8f425478515e59b04cc77b0ce9ab392f89cf7
TheaLikesCode <thea.alnaes@gmail.com>
2017-11-16 22:05:51 +0100

    Removed useless code
    Removed old code and debug printing + whitespaces

--- SkynetRevolution1_Dijkstra.cs
+++ SkynetRevolution1_Dijkstra.cs
@@ -22,7 +22,6 @@ class Player
         public int ID;
         public List<int> neighbours;
         public bool isGateway;
-        public int weight = 1;
         
         public List<int> GetNeighbours(){
             return neighbours;
@@ -47,92 +46,72 @@ class Player
         List<Node> exits = new List<Node>();
         
         for(int i =0; i < numberOfNodes; i++){
-            nodes.Add(new Node(i));
-            
+            nodes.Add(new Node(i));           
         }
+
         for (int i = 0; i < numberOfLinks; i++)
         {
             inputs = Console.ReadLine().Split(' ');
             int N1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
             int N2 = int.Parse(inputs[1]);
             
-            Console.Error.WriteLine("Node " + N1 + " links with " + N2);
-            
             nodes[N1].AddNeighbour(N2);
             nodes[N2].AddNeighbour(N1);
         }
         
-        
-       
         for (int i = 0; i < numberOfExitGateways; i++)
         {
             int EI = int.Parse(Console.ReadLine()); // the index of a gateway node
             
-          var gateways = nodes.Where(n=> n.ID==EI);
+            var gateways = nodes.Where(n=> n.ID==EI);
           
-          foreach(var n in gateways){
+            foreach(var n in gateways){
               n.SetGateway(true);
               exits.Add(n);
           }
           
         }
-         Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+
+        Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
         foreach(Node n in nodes){
             graph[n.ID] = n.GetNeighbours();
-        }
-        
+        }       
 
         // game loop
         while (true)
         {
-            int SI = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
-           
+            int SI = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn          
            
             int shortestDist = int.MaxValue;
             List<int> shortestPath = null;
-            Dictionary<int,int> nodePriority = new Dictionary<int,int>();
             
-            for( int i = 0; i < numberOfNodes; i++ )
-            {
-                nodePriority.Add( i, 0 );
-            }
-            
-            for( int i = 0; i < numberOfExitGateways; i++ )
+            for(int i = 0; i < numberOfExitGateways; i++)
             {
               List<int> path = FindShortestPath(graph, SI, exits[i].ID);
               
-                if( path != null && path.Count > 0 )
+                if(path != null && path.Count > 0)
                 {
-                    if( path.Count < shortestDist )
+                    if(path.Count < shortestDist)
                     {
                         shortestDist = path.Count;
                         shortestPath = path;
                     }
                 }
             }
-                int start = SI, end = 0;
-                for( int i = 0; i < shortestPath.Count; i++ )
-                {
-                   
-                    start = i == 0 ? SI : shortestPath[i-1];
-                    end = shortestPath[i];
-                    break;
-                    
-                }
+
+                int start = SI, end = shortestPath.FirstOrDefault();
                 Console.WriteLine(start + " " + end);
- 
         }
     }
     
-    public static List<int> FindShortestPath(  Dictionary<int, List<int>> graph , int start, int finish)
+    public static List<int> FindShortestPath(Dictionary<int, List<int>> graph , int start, int finish)
     {
-     
         List<int> nodes = new List<int>();
         Dictionary<int, int> distances = new Dictionary<int,int>();
         Dictionary<int, int> previous = new Dictionary<int, int>();
         
         
-        for( int i = 0; i < graph.Count; i++ )
+        for(int i = 0; i < graph.Count; i++)
         {
             nodes.Add(i);
             distances[i] = int.MaxValue; //set to infinity
@@ -142,19 +121,17 @@ class Player
         
         List<int> path = new List<int>();
         
-        while( nodes.Count != 0 )
+        while(nodes.Count != 0)
         { 
-           nodes.Sort( ( a, b ) => distances[a] - distances[b] );
-    
-  
-            
+           nodes.Sort((a, b) => distances[a] - distances[b] );
+
             int closest = nodes[0];
-            nodes.RemoveAt( 0 );
+            nodes.RemoveAt(0);
             
-            if( closest == finish )
+            if(closest == finish)
             {
                  // Found the shortest path
-                while( previous.ContainsKey( closest ) )
+                while(previous.ContainsKey(closest))
                 {
                     path.Add(closest);
                     closest = previous[closest];
@@ -162,15 +139,15 @@ class Player
                 break;
             }
             
-            if( distances[closest] == int.MaxValue )
+            if(distances[closest] == int.MaxValue)
             {
                 return null;
             }
             
              // Calculate distances
-            foreach( int neighbor in graph[closest] )
+            foreach(int neighbor in graph[closest])
             {
-                if( distances[closest] + 1 < distances[neighbor] )
+                if(distances[closest] + 1 < distances[neighbor])
                 {
                     distances[neighbor] = distances[closest] + 1;
                     previous[neighbor] = closest;
